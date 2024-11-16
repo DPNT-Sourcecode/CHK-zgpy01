bff19af46dee363c85f6daff13f771b13c312c73
sbx_user1051 <sbx_user1051@169.254.21.29>
2024-11-16 01:15:38 +0000

    Sat Nov 16 01:15:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,22 +2,31 @@
 # skus = unicode string
 def checkout(skus):
     prices = {
-        'A': 50,
-        'B': 30,
-        'C': 20,
-        'D': 15,
-        'E': 40,
-        'F': 10,
+        'A': 50,  'B': 30,  'C': 20,  'D': 15,  'E': 40,  'F': 10,
+        'G': 20,  'H': 10,  'I': 35,  'J': 60,  'K': 80,  'L': 90,
+        'M': 15,  'N': 40,  'O': 10,  'P': 50,  'Q': 30,  'R': 50,
+        'S': 30,  'T': 20,  'U': 40,  'V': 50,  'W': 20,  'X': 90,
+        'Y': 10,  'Z': 50,
     }
+
 
     offers = {
         'A': [(5, 200), (3, 130)],
         'B': [(2, 45)],
+        'H': [(10, 80), (5, 45)],
+        'K': [(2, 150)],
+        'P': [(5, 200)],
+        'Q': [(3, 80)],
+        'V': [(3, 130), (2, 90)],
     }
+
 
     free_offers = {
         'E': ('B', 2),
         'F': ('F', 2),
+        'N': ('M', 3),
+        'R': ('Q', 3),
+        'U': ('U', 3),
     }
 
     if not isinstance(skus, str) or not all(char in prices for char in skus):
@@ -51,4 +60,5 @@ def checkout(skus):
         else:
             total += count * prices[sku]
     return total
+
 

2d9a3cd01efce106469feefe3e56630405a30cf3
sbx_user1051 <sbx_user1051@169.254.21.29>
2024-11-16 01:44:58 +0000

    Sat Nov 16 01:44:58 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,9 +3,7 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        # assert checkout_solution.checkout('HHHHH') == 45
-        # assert checkout_solution.checkout('VV') == 90
-        # assert checkout_solution.checkout('ABCDX') == 225
         assert checkout_solution.checkout('STXYZ') == 82
-
+        assert checkout_solution.checkout('SSSXXXYYYZZZ') == 180
+        assert checkout_solution.checkout('') == 0
 
